701e261d3e53ce03ed38b1b5906afd41a36d30e2
sanaullah <sanaullahhaifz@gmail.com>
2024-06-06 22:51:14 +0400

    db url update

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_DB_NAME = process.env.DB_NAME || "";
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.wc4xq.mongodb.net/`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.aeurg0f.mongodb.net/`;
 
 const SERVER_PORT = process.env.PORT || 8001;
 const JWT_AUTH_SECRET = process.env.JWT_AUTH_SECRET || "";
